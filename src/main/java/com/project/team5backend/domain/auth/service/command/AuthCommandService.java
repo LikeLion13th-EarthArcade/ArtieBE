68fc8eb0305dd2b2568a4ca48728767012164d11
Gyueon Lee <gon2yq@naver.com>
2025-09-07 19:02:11 +0900

    ✨feat: 비밀번호 변경 구현

--- src/main/java/com/project/team5backend/domain/auth/service/command/AuthCommandService.java
+++ src/main/java/com/project/team5backend/domain/auth/service/command/AuthCommandService.java
@@ -1,8 +1,11 @@
 package com.project.team5backend.domain.auth.service.command;
 
+import com.project.team5backend.domain.auth.dto.request.AuthReqDTO;
 import com.project.team5backend.domain.user.entity.User;
 
 public interface AuthCommandService {
 
     void savePassword(User user, String encodedPassword);
+
+    void changePassword(long id, AuthReqDTO.AuthPasswordChangeReqDTO authPasswordChangeReqDTO);
 }
