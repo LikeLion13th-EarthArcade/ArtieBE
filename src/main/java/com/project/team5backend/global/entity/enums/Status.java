21db801a91475e2264e7ec5e1a61066f392dd470
Gyueon Lee <gon2yq@naver.com>
2025-09-10 14:01:05 +0900

    ✨feat: Status enum 수정

--- src/main/java/com/project/team5backend/global/entity/enums/Status.java
+++ src/main/java/com/project/team5backend/global/entity/enums/Status.java
@@ -1,9 +1,10 @@
 package com.project.team5backend.global.entity.enums;
 
 public enum Status {
-    APPROVED, // 승인
-    PENDING,  // 심사중
-    REJECTED,  // 거절
-    CANCELED  // 취소
+    NEW,        // 예약 생성 예약금 미입금
+    PENDING,    // 입금 완료된 상태, 호스트 검색가능      & 전시/공간 심사중
+    APPROVED,   // 호스트의 예약 확정                   & 관리자의 전시/공간 승인
+    REJECTED,   // 거절
+    CANCELED
     ;
 }
