0a013ab6c9acd6af6bcef0ad2e6829c993c7c0a7
seohyunseok <tjgustjr33@gmail.com>
2025-09-24 20:14:49 +0900

    ♻️ refactor: 컨버터 접근 private 설정

--- src/main/java/com/project/team5backend/domain/admin/dashboard/service/query/AdminDashboardQueryServiceImpl.java
+++ src/main/java/com/project/team5backend/domain/admin/dashboard/service/query/AdminDashboardQueryServiceImpl.java
@@ -1,12 +1,17 @@
 package com.project.team5backend.domain.admin.dashboard.service.query;
 
+import com.project.team5backend.domain.admin.dashboard.converter.AdminDashboardConverter;
 import com.project.team5backend.domain.admin.dashboard.dto.response.AdminDashboardResDTO;
+import com.project.team5backend.domain.admin.dashboard.dto.response.ExhibitionSummaryResDTO;
+import com.project.team5backend.domain.admin.dashboard.dto.response.SpaceSummaryResDTO;
 import com.project.team5backend.domain.exhibition.repository.ExhibitionRepository;
 import com.project.team5backend.domain.space.repository.SpaceRepository;
 import com.project.team5backend.global.entity.enums.Status;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -17,13 +22,20 @@ public class AdminDashboardQueryServiceImpl implements AdminDashboardQueryServic
     private final SpaceRepository spaceRepository;
     @Override
     public AdminDashboardResDTO.AdminDashboardSummaryResDTO getAdminDashboard() {
-        return AdminDashboardResDTO.AdminDashboardSummaryResDTO.builder()
-                .pendingExhibitionCount(exhibitionRepository.findPendingExhibitionsCountByStatus(Status.PENDING))
-                .pendingSpaceCount(spaceRepository.findPendingSpacesCountByStatus(Status.PENDING))
-                .totalExhibitionCount(exhibitionRepository.count())
-                .totalSpaceCount(spaceRepository.count())
-                .pendingExhibitions(exhibitionRepository.findTop3ByStatus(Status.PENDING))
-                .pendingSpaces(spaceRepository.findTop3ByStatus(Status.PENDING))
-                .build();
+        long pendingExhibitionCount = exhibitionRepository.findPendingExhibitionsCountByStatus(Status.PENDING);
+        long pendingSpaceCount = spaceRepository.findPendingSpacesCountByStatus(Status.PENDING);
+        long totalExhibitionCount = exhibitionRepository.count();
+        long totalSpaceCount = spaceRepository.count();
+        List<ExhibitionSummaryResDTO> pendingExhibitions = exhibitionRepository.findTop3ByStatus(Status.PENDING);
+        List<SpaceSummaryResDTO> pendingSpaces = spaceRepository.findTop3ByStatus(Status.PENDING);
+
+        return AdminDashboardConverter.toSummaryResDTO(
+                pendingExhibitionCount,
+                pendingSpaceCount,
+                totalExhibitionCount,
+                totalSpaceCount,
+                pendingExhibitions,
+                pendingSpaces
+        );
     }
 }
