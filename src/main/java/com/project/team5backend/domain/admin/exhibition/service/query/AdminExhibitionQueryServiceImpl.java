a83453f27964f1612799fd5af16248d4630f3f4f
Gyueon Lee <gon2yq@naver.com>
2025-11-07 00:10:46 +0900

    ♻️refactor: 전시 admin private 메서드로 분리

--- src/main/java/com/project/team5backend/domain/admin/exhibition/service/query/AdminExhibitionQueryServiceImpl.java
+++ src/main/java/com/project/team5backend/domain/admin/exhibition/service/query/AdminExhibitionQueryServiceImpl.java
@@ -41,8 +41,7 @@ public class AdminExhibitionQueryServiceImpl implements AdminExhibitionQueryServ
 
     @Override
     public AdminExhibitionResDTO.ExhibitionDetailResDTO getDetailExhibition(long exhibitionId) {
-        Exhibition exhibition = exhibitionRepository.findByIdAndIsDeletedFalse(exhibitionId)
-                .orElseThrow(() -> new ExhibitionException(ExhibitionErrorCode.EXHIBITION_NOT_FOUND));
+        Exhibition exhibition = getExhibition(exhibitionId);
 
         List<String> imageUrls = exhibitionImageRepository.findImageUrlsByExhibitionId(exhibitionId).stream()
                 .map(fileUrlResolverPort::toFileUrl)
@@ -50,5 +49,10 @@ public class AdminExhibitionQueryServiceImpl implements AdminExhibitionQueryServ
 
         return AdminExhibitionConverter.toExhibitionDetailResDTO(exhibition, imageUrls);
 
+    }
+
+    private Exhibition getExhibition(long exhibitionId) {
+        return exhibitionRepository.findByIdAndIsDeletedFalse(exhibitionId)
+                .orElseThrow(() -> new ExhibitionException(ExhibitionErrorCode.EXHIBITION_NOT_FOUND));
     }
 }
